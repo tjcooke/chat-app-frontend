3314d0e9e9f5c11fc4916032078545edc3a8b28a
Tyler Cooke <tylerstouchpad@gmail.com>
2019-05-02 15:41:47 -0400

    form calls handler functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import ChatList from './ChatList';
+import ChatForm from './ChatForm';
+import { throwStatement } from '@babel/types';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    constructor(props) {
+      super(props);
+      this.state = {
+        messages: [],
+        text: ''
+      };
+    }
+
+    async componentDidMount() {
+
+      setInterval(async () => {
+        const {data} = await axios.get('/api')
+        console.log(data);
+        this.setState({
+          messages: data
+
+      })
+
+    }, 2000);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Chat App</h1>
+        <ChatList messages={this.state.messages}></ChatList>
+        <ChatForm
+          text={this.state.text}
+          handleChange={this._setText}
+          handleSend={throwStatement._sendMessage}></ChatForm>
+      </div>
+    );
+  }
+
+  _setText = () => {
+    console.log('App _setText got called');
+
+  }
+
+  _sendMessage = () => {
+    console.log('App _sendMessage got called');
+
+  }
 }
 
 export default App;
